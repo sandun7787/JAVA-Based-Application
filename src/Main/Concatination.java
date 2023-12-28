8ea04c193abf647cb269b12e32693dda443ebe01
sandun7787 <sandunwarnasooreya345@gmail.com>
2023-12-28 16:54:17 +0530

    create old

---
+++ src/Main/Concatination.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Concatination {
+
+	public static void main(String[]args) {
+		String fName="Sandun";
+		String lName="Warnasooriya";
+		System.out.println(fName+""+lName);
+	}
+}
---
+++ src/Main/Equaloperator.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Equaloperator {
+
+	public static void main(String []args) {
+		int x= 10;
+		int y=34;
+		System.out.println(x==y);
+	}
+}

b3987172f3b6903dd6b999e6c6970b7288879d88
sandun7787 <sandunwarnasooreya345@gmail.com>
2023-12-31 13:01:36 +0530

    create data based

---
+++ src/Main/ChangetoArrayElement.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class ChangetoArrayElement {
+	public static void main(String []args) {
+		String[]cars= {"Volvo", "BMW", "Ford", "Mazda"};
+		cars[3]="opel";
+		System.out.println(cars[3]);
+	}
+
+}

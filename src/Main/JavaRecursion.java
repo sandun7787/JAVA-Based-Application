f07c4da821601ec746d06f8c72e8d66314d34f6d
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-01-02 08:47:20 +0530

    add new recursion

---
+++ src/Main/JavaRecursion.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class JavaRecursion {
+	
+	public static void main(String[]args) {
+		int result=sum(10);
+		System.out.println(result);
+	}
+
+	public static int  sum (int k) {
+		if(k>0) {
+			return k+sum(k-1);
+		}else {
+			return 0;
+		}
+		
+		
+	}
+}

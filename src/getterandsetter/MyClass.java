3d2bd8aa420fd2ffaafb7f55c95940cd59859394
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-01-03 14:48:25 +0530

    new/data/based/add

---
+++ src/getterandsetter/MyClass.java
@@ -0,0 +1,10 @@
+package getterandsetter;
+
+public class MyClass {
+	public static void main(String []args) {
+		Person my = new Person();
+	    my.setName("sanun");
+	    System.out.println(my.getName());
+	}
+
+}
---
+++ src/getterandsetter/Person.java
@@ -0,0 +1,17 @@
+package getterandsetter;
+
+public class Person {
+	
+	private String name;
+	
+	//Getter
+	public String getName() {
+		return name;
+	}
+	
+	//Setter
+	public void setName(String newName) {
+		 this.name = newName;
+	}
+
+}

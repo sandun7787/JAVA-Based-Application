196b4490d9270bf9c353e536b59115c7dcafba04
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-01-06 15:31:37 +0530

    update-the-home-page

---
+++ src/Main/kka.java
@@ -0,0 +1,10 @@
+package Main;
+import java.time.LocalDate;// import the localdata class
+
+public class kka {
+
+	public static void main(String [] args) {
+		LocalDate my = LocalDate.now();
+		System.out.println(my);
+	}
+}

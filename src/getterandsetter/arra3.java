d117f0c9afc4acf7f3614fc22380a343b930e493
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-01-08 09:38:36 +0530

    add/new/interface

---
+++ src/getterandsetter/arra3.java
@@ -0,0 +1,12 @@
+package getterandsetter;
+import java.util.ArrayList;
+public class arra3 {
+	public static void main(String [] args) {
+		ArrayList<String> ca = new ArrayList<String>();
+		ca.add("dsfs");
+		ca.add("fgg");
+		ca.add("gdg");
+		System.out.println(ca.size());
+	}
+
+}
---
+++ src/getterandsetter/array1.java
@@ -0,0 +1,13 @@
+package getterandsetter;
+import java.util.ArrayList;
+public class array1 {
+	public static void main(String []args) {
+		ArrayList<String> car = new ArrayList<String>();
+		car.add("ll");
+		car.add("fjksafj");
+		car.add("ffffffg");
+		car.remove(0);
+		System.out.println(car);
+	}
+
+}
